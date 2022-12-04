4e1b772267b8a7ab948eaa49b197bb1ec370b01c
David López <davidlpzgomez@gmail.com>
2022-12-04 16:31:20 +0100

    Add Day 03 bonus transition

--- 03-xmas-countdown/src/components/CountdownSegment.vue
+++ 03-xmas-countdown/src/components/CountdownSegment.vue
@@ -1,18 +1,22 @@
+<template>
+  <div class="segment text-center">
+    <div class="pt-10 overflow-hidden relative">
+      <transition name="move">
+        <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      </transition>
+    </div>
+
+    <span class="label block pt-2">{{ label }}</span>
+  </div>
+</template>
+
 <script setup>
 defineProps({
   label: String,
   number: Number,
 })
 </script>
-<template>
-  <div class="segment text-center">
-    <div class="pt-10 overflow-hidden relative">
-      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
-    </div>
 
-    <span class="label block pt-2">{{ label }}</span>
-  </div>
-</template>
 <style>
 .segment {
   width: 80px;
@@ -23,5 +27,19 @@ defineProps({
 }
 .label {
   font-size: 16px;
+}
+.move-enter-active,
+.move-leave-active {
+  transition: all .5s ease-in
+}
+.move-enter-from {
+  transform: translateY(-100%) translateX(-50%);
+}
+.move-leave-to {
+  transform: translateY(100%) translateX(-50%);
+}
+.move-enter-to,
+.move-leave-from {
+  transform: translateY(0px) translateX(-50%);
 }
 </style>
