e2554ef5f381a85543ea9e4062aea615b29c16b9
David López <davidlpzgomez@gmail.com>
2022-12-04 16:27:09 +0100

    Add Day 03 solution

--- 03-xmas-countdown/src/App.vue
+++ 03-xmas-countdown/src/App.vue
@@ -1,21 +1,13 @@
-<script setup>
-import CountdownHeader from '@/components/CountdownHeader.vue'
-import CountdownSegment from './components/CountdownSegment.vue'
-import { useNow } from '@vueuse/core'
-
-const now = useNow()
-const christmas = new Date('12/25/2022 00:00:00')
-</script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
     <div>
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" number="00" />
-          <CountdownSegment label="hours" number="00" />
-          <CountdownSegment label="minutes" number="00" />
-          <CountdownSegment label="seconds" number="00" />
+          <CountdownSegment label="days" :number="days" />
+          <CountdownSegment label="hours" :number="hours" />
+          <CountdownSegment label="minutes" :number="minutes" />
+          <CountdownSegment label="seconds" :number="seconds" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
@@ -24,6 +16,34 @@ const christmas = new Date('12/25/2022 00:00:00')
     </div>
   </div>
 </template>
+
+<script setup>
+import CountdownHeader from '@/components/CountdownHeader.vue';
+import CountdownSegment from '@/components/CountdownSegment.vue';
+import { computed } from 'vue';
+import { useNow } from '@vueuse/core';
+
+const now = useNow();
+const christmas = new Date('12/25/2022 00:00:00');
+
+const countdown = computed(() => christmas.getTime() - now.value.getTime());
+
+const days = computed(() => {
+  return Math.trunc(countdown.value / (1000*60*60*24));
+});
+
+const hours = computed(() => {
+  return Math.trunc((countdown.value / (1000*60*60)) - (days.value*24));
+});
+
+const minutes = computed(() => {
+  return Math.trunc((countdown.value / (1000*60)) - (days.value*24*60) - (hours.value*60));
+});
+
+const seconds = computed(() => {
+  return Math.trunc((countdown.value / (1000)) - (days.value*24*60*60) - (hours.value*60*60) - (minutes.value*60));
+});
+</script>
 
 <style>
 div {
