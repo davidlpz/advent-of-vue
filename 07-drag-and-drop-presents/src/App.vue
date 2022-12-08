6126c1707ce316148be9583d4cbca046731c4647
David López <davidlpzgomez@gmail.com>
2022-12-08 17:37:17 +0100

    Add Day 07 solution

--- 07-drag-and-drop-presents/src/App.vue
+++ 07-drag-and-drop-presents/src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="flex flex-col items-center mt-24 min-h-screen w-full">
     <h1 class="mt-8 text-xl font-bold">Drag the presents under the tree!</h1>
-    <!-- TODO: make ChristmasTree our drop zone! -->
-    <ChristmasTree :presents="underTheTree" class="mt-16" />
+    <ChristmasTree 
+      :presents="underTheTree" 
+      class="mt-16" 
+      @drop="onDrop($event)" 
+      @dragover.prevent 
+      @dragenter.prevent
+    />
     <div class="pt-32 mt-32 bg-gray-100 w-full justify-center flex-1">
       <div class="flex items-end justify-center" v-auto-animate>
-        <!-- TODO: make each present draggable -->
-        <ChristmasPresent v-for="(p, index) in presents" :key="p" :name="p" />
+        <ChristmasPresent 
+          v-for="(p, index) in presents" 
+          :key="p" 
+          :name="p" 
+          draggable="true"
+          @dragstart="startDrag($event, index)" 
+        />
       </div>
     </div>
   </div>
@@ -17,14 +27,18 @@ import ChristmasPresent from './components/ChristmasPresent.vue';
 import ChristmasTree from './components/ChristmasTree.vue';
 import { ref } from 'vue';
 
-const presents = ref(['small-red-gift', 'blue-gift', 'tall-red-gift'])
-const underTheTree = ref([])
+const presents = ref(['small-red-gift', 'blue-gift', 'tall-red-gift']);
+const underTheTree = ref([]);
 
 const startDrag = (evt, index) => {
-  // TODO
+  evt.dataTransfer.dropEffect = 'move'
+  evt.dataTransfer.effectAllowed = 'move'
+  evt.dataTransfer.setData('itemIndex', index)
 }
 
 const onDrop = evt => {
-  // TODO
+  const itemIndex = evt.dataTransfer.getData('itemIndex')
+  underTheTree.value.push(presents.value[itemIndex]);
+  presents.value.splice(itemIndex, 1);
 }
 </script>
